5f73516142e605bc1e27cdee27b653e13f7db982
RPI <cjan957@aucklanduni.ac.nz>
2018-07-10 14:51:25 +1200

    Can now print number of cycles

--- detect_vehicle.cpp
+++ detect_vehicle.cpp
@@ -23,6 +23,21 @@
 #include <iomanip>
 #include <sstream>
 
+// -- Timing --
+
+#include <linux/perf_event.h>
+#include <linux/hw_breakpoint.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <asm/unistd.h>
+
+long long inst_count;
+int inst_fd;
+
+// -------------------------
 
 using namespace cv;
 using namespace cv::ml;
@@ -31,14 +46,14 @@ using namespace std;
 #define YML_LOCATION "vehicle_detector_no_sobel.yml"
 #define IMAGE_SIZE Size(64,64)
 
-#define SEQUENCE_SET 0 // 1 for sequence of images, 0 for a single shot (include L and R), specify below
+#define SEQUENCE_SET 1 // 1 for sequence of images, 0 for a single shot (include L and R), specify below
 #define CAMERA_1_LOCATION "/home/pi/Desktop/700/stereo_dataset/resize_left/I1_000163.png"
 #define CAMERA_2_LOCATION "/home/pi/Desktop/700/stereo_dataset/resize_right/I2_000163.png"
 #define START_AT_SEQUENCE 100 // <- specify STARTING SEQUENCE HERE
 #define CONFIDENCE_THRESHOLD 0.5 //specify confidence threshold for when by pass is 0
 #define BYPASS_CONFIDENCE_CHECK 0 //draw black boxes when not confidence
 
-#define LOOP 1
+#define LOOP 0
 
 
 
@@ -48,6 +63,10 @@ Point getPoints(Mat &image, vector<Rect> &detections, vector<double> &foundWeigh
 
 
 float disparityMap(Mat imageL, Mat imageR, vector<Rect> &detections_L, vector<double> &foundWeights_L, vector<Rect> &detections_R, vector<double> &foundWeights_R);
+
+static void setup_counters(void);
+static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags);
 
 int main()
 {	 
@@ -129,6 +148,10 @@ int main()
 	string direct= "/home/pi/Desktop/700/stereo_dataset/resize_left/";
 	string direct_right = "/home/pi/Desktop/700/stereo_dataset/resize_right/";
 
+	cout << "TIMER STARTED " << endl;
+	
+	setup_counters();
+	
 	//change starting image by changing i?
 	for (i = START_AT_SEQUENCE; i < fileCount; i++) 
 	{
@@ -224,10 +247,17 @@ int main()
 		{ 
 			return 1;
 		}
-		
-
-		
+				
 	}
+	
+	ioctl(inst_fd, PERF_EVENT_IOC_DISABLE, 0);
+    read(inst_fd, &inst_count, sizeof(long long));
+
+    printf("Used %lld instructions\n", inst_count);
+
+    close(inst_fd);	
+	
+	
 	
 #else
 	
@@ -433,3 +463,42 @@ vector<float> get_svm_detector(const Ptr<SVM> &svm)
 	
 	return hog_detector;
 }
+
+static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags)
+{
+    int ret;
+
+    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu,
+                   group_fd, flags);
+    return ret;
+}
+
+
+static void setup_counters(void)
+{
+	
+    struct perf_event_attr pe;
+
+    memset(&pe, 0, sizeof(struct perf_event_attr));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_CPU_CYCLES;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
+
+    inst_fd = perf_event_open(&pe, 0, -1, -1, 0);
+    if (inst_fd == -1) {
+       fprintf(stderr, "Error opening leader %llx\n", pe.config);
+       exit(EXIT_FAILURE);
+    }
+    
+    ioctl(inst_fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(inst_fd, PERF_EVENT_IOC_ENABLE, 0);
+	
+}
+	
+	
+	
+	
