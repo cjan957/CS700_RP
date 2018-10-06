917b71f797beddd12312e17b74b909495baa14a7
Chanokpol <cjan957@aucklanduni.ac.nz>
2018-10-06 15:54:55 +1300

    Generating the files for images

---
+++ Distance Measurement/Disparity_Images.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/ml.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include <opencv2/calib3d.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/ximgproc/disparity_filter.hpp"
+
+#include <fstream>
+#include <algorithm>
+#include <cstdlib>
+
+#include <iomanip>
+#include <sstream>
+#include <stdlib.h>
+#include <math.h>
+#include <cstdint>
+#include <stdio.h>
+#include <dirent.h>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+Mat disp_GT, leftImg, rightImg, depthMap;
+
+cv::Mat DepthMap(cv::Mat &imageL, cv::Mat &imageR)
+{
+
+    /// DISPARITY MAP AND DEPTH MAP
+    Mat left_for_matcher = imageL;
+    Mat right_for_matcher = imageR;
+    Mat left_disp,right_disp;
+    Mat filtered_disp;
+    int max_disp = 64; // n*16
+    int wsize = 15;
+
+	//~ max_disp/=2;
+    //~ if(max_disp%16!=0)
+    //~ {
+		//~ max_disp += 16-(max_disp%16);
+    //~ }
+        
+    // resize(imageL ,left_for_matcher ,Size(),0.5,0.5);
+	// resize(imageR, right_for_matcher,Size(),0.5,0.5);
+
+
+   // Perform matching and create the filter instance
+   /* I am using StereoBM for faster processing. If speed is not critical, 
+   though, StereoSGBM would provide better quality.
+   The filter instance is created by providing the StereoMatcher instance
+   that we intend to use. Another matcher instance is returned by the
+   createRightMatcher function. These two matcher instances are then used
+   to compute disparity maps both for the left and right views, that are
+   required by the filter. */
+
+    cv::Ptr<cv::ximgproc::DisparityWLSFilter> wls_filter;
+
+    // BM
+    //~ cv::Ptr<cv::StereoBM >left_matcher = cv::StereoBM::create(max_disp,wsize);
+    
+    // SGBM
+    Ptr<StereoSGBM> left_matcher  = StereoSGBM::create(0,max_disp,wsize);
+    left_matcher->setMode(StereoSGBM::MODE_SGBM);
+    
+    wls_filter = cv::ximgproc::createDisparityWLSFilter(left_matcher);
+    cv::Ptr<cv::StereoMatcher> right_matcher = cv::ximgproc::createRightMatcher(left_matcher);
+
+	left_matcher-> compute(left_for_matcher, right_for_matcher,left_disp);
+    right_matcher->compute(right_for_matcher,left_for_matcher, right_disp);
+
+    // Perform filtering
+    /* Disparity maps computed by the respective matcher instances, as
+    well as the source left view are passed to the filter. Note that we
+    are using the original non-downscaled view to guide the filtering 
+    process. 
+    The disparity map is automatically upscaled in an edge-aware fashion
+    to match the original view resolution. The result is stored in
+    filtered_disp. */
+
+
+    double lambda = 8000.0;   // hardcode
+    double sigma = 2;       // hardcode
+	int vis_mult = 2;
+
+    //! [filtering]
+    wls_filter->setLambda(lambda);
+    wls_filter->setSigmaColor(sigma);
+
+    wls_filter->filter(left_disp, imageL, filtered_disp, right_disp);
+
+    cv::Mat raw_disp_vis;
+    cv::ximgproc::getDisparityVis(left_disp,raw_disp_vis, vis_mult);
+    cv::Mat filtered_disp_vis;
+    cv::ximgproc::getDisparityVis(filtered_disp,filtered_disp_vis, vis_mult);
+
+
+    return filtered_disp_vis;  
+}
+
+
+int main()
+{
+
+	int max_disp = 64; // n*16
+    int wsize = 15;
+			
+	leftImg = imread("/home/pi/Desktop/CS700_RP/stereo_dataset/city/left/0000000044.png", IMREAD_ANYDEPTH);
+	rightImg = imread("/home/pi/Desktop/CS700_RP/stereo_dataset/city/right/0000000044.png", IMREAD_ANYDEPTH);
+					
+	Mat disp8;
+	
+	Mat depthMap = Mat(leftImg.size().height, leftImg.size().width, CV_16S);
+	
+	Ptr<StereoSGBM> sbm  = StereoSGBM::create(0,max_disp,wsize);
+
+	sbm->compute(leftImg, rightImg, depthMap);		
+	
+	normalize(depthMap, disp8, 0, 255, CV_MINMAX, CV_8U);
+		
+	//~ imshow("TEST", disp8);
+	
+	//~ waitKey(0);
+	imwrite("disparity_SGBM.png", depthMap);
+							
+				
+
+}
--- Distance Measurement/disparity.sh
+++ Distance Measurement/disparity.sh
@@ -1,5 +1,5 @@
 echo Compiling..
 
-g++ $(pkg-config --libs --cflags opencv) -o disp_EVAL Disparity_EVAL.cpp
+g++ $(pkg-config --libs --cflags opencv) -o disp_IMAGE Disparity_Images.cpp
 
-./disp_EVAL
+./disp_IMAGE
